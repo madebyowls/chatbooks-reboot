a8bbb5366b3cbc4f3f0530334ea5901a8b49c065
Nick Christensen <nick.g.christensen@gmail.com>
2014-02-27 00:14:44 -0700

    Fix for android logo, shifted iphone

--- assets/chatbooks.js
+++ assets/chatbooks.js
@@ -2,5 +2,17 @@
  * SVGeezy.js 1.0
 */
 var svgeezy=function(){return{init:function(a,b){this.avoid=a||!1,this.filetype=b||"png",this.svgSupport=this.supportsSvg(),this.svgSupport||(this.images=document.getElementsByTagName("img"),this.imgL=this.images.length,this.fallbacks())},fallbacks:function(){for(;this.imgL--;)if(!this.hasClass(this.images[this.imgL],this.avoid)||!this.avoid){var a=this.images[this.imgL].getAttribute("src");if(null===a)continue;if("svg"==this.getFileExt(a)){var b=a.replace(".svg","."+this.filetype);this.images[this.imgL].setAttribute("src",b)}}},getFileExt:function(a){var b=a.split(".").pop();return-1!==b.indexOf("?")&&(b=b.split("?")[0]),b},hasClass:function(a,b){return(" "+a.className+" ").indexOf(" "+b+" ")>-1},supportsSvg:function(){return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1")}}}();
-
 svgeezy.init(false, 'png');
+
+// Android Waiting List Submission
+// var data = { email: 'email@domain.com' };
+// $.ajax({
+//	type: 'post',
+//	url: 'http://chatbooks.com/waitinglist',
+//	data: JSON.stringify(data),
+//	dataType: 'json',
+//	contentType: 'application/json; charset=utf-8',
+//	success: function (result) {
+
+//	}
+// });
